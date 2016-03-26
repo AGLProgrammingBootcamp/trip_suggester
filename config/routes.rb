4db4871c8b3a3291b0315eb09e54454ee34eb16f
H.Yoshiki <vitam.regit.fortuna@gmail.com>
2016-03-26 12:38:06 +0000

    switched root to direction#create from finders#index. これからokamoto view baseで作業しま

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-#  root 'direction#create'
-  root 'finders#index'
+  root 'direction#create'
+#  root 'finders#index'
 
 #  get '/direction/hotels' => 'direction#hotels'
 #  post '/direction/new' => 'direction#new'
