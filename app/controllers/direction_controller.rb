8721355428eb1c450f7fb5b35ae706f48982287b
H.Yoshiki <vitam.regit.fortuna@gmail.com>
2016-03-27 04:44:33 +0000

    prepared for merge to master

--- app/controllers/direction_controller.rb
+++ app/controllers/direction_controller.rb
@@ -46,11 +46,6 @@ require 'mechanize'
     @elements = page.search('ul li li.fare')
     @fare = @elements[0].inner_text
     @elements = page.search('dl dt a')
-    # @transfers = @elements[1].inner_text
-    # @elemtnt= @elemtnt.inner_text
-    # @elements.each do |ele|
-    #   @element=ele.inner_text
-    # end
   end
 
   def start
@@ -132,8 +127,9 @@ puts "@arr is #{@arr}"
       @address_dep=params[:dept]
       
       @dep = params[:dept]
-      @move_fares=Array.new()
       @arrive=Array.new()
+      @arrive << @arr
+      @move_fares=Array.new()
 
       agent = Mechanize.new
       @url="http://jws.jalan.net/APIAdvance/HotelSearch/V1/?key="+@api_key+"&s_area="+@s_area+"&max_rate="+@max_rate+"&min_rate="+@min_rate+"&count=2&xml_ptn=1"
@@ -148,42 +144,18 @@ puts "@arr is #{@arr}"
       @hotel_sampleratefrom = page.search('SampleRateFrom')
   
       @hotel_name.zip(@hotel_address,@hotel_sampleratefrom).each do|name,address,sampleratefrom|
-        #地名から緯度・経度を検索
-       # agent1 = Mechanize.new
-      #  page1 = agent1.get("http://www.geocoding.jp/api/?v=1.1&q="+@address_dep)
-      #  @elements1=page1
-      #  @lat1=page1.search('lat').inner_text
-      #  @lng1=page1.search('lng').inner_text
-  
-        # agent2 = Mechanize.new
-        # @address_app=address.inner_text
-        # page2 = agent2.get("http://www.geocoding.jp/api/?v=1.1&q="+@address_app)
-        # if page2.search('error').inner_text!="003"
-        #   @lat2=page2.search('lat').inner_text
-        #   @lng2=page2.search('lng').inner_text
-         #緯度経度から最寄り駅を検索
-      #     agent1 = Mechanize.new
-      #    page1 = agent1.get("http://map.simpleapi.net/stationapi?x="+@lng1+"&y="+@lat1)
-      #     @dep = page1.at('station name').inner_text
-        #   agent4 = Mechanize.new
-        #   page2 = agent4.get("http://map.simpleapi.net/stationapi?x="+@lng2+"&y="+@lat2)
-        #   @arr = page2.at('station name').inner_text
-          @arrive << @arr
-          #最寄り駅から費用を計算
-          agent = Mechanize.new
-          page = agent.get("http://transit.yahoo.co.jp/search/result?flatlon=&from="+@dep+"&tlatlon=&to="+@arr+"&via=&via=&via=&y=2016&m=03&d=25&hh=16&m2=4&m1=3&type=1&ticket=ic&al=1&shin=1&ex=1&hb=1&lb=1&sr=1&s=0&expkind=1&ws=2")
+      #地名から緯度・経度を検索
+      
+      #駅名から運賃を計算
+        agent = Mechanize.new
+        page = agent.get("http://transit.yahoo.co.jp/search/result?flatlon=&from="+@dep+"&tlatlon=&to="+@arr+"&via=&via=&via=&y=2016&m=03&d=25&hh=16&m2=4&m1=3&type=1&ticket=ic&al=1&shin=1&ex=1&hb=1&lb=1&sr=1&s=0&expkind=1&ws=2")
 puts "search for trains done from #{@dep} to #{@arr}."
-          @elements = page.search('ul li li.fare')
+        @elements = page.search('ul li li.fare')
 puts "Result price was #{@elements[0].inner_text}."
-          @fare = @elements[0].inner_text.delete("円").delete(",")
-          @fare = @fare.to_i
-          @fare_return = @fare *2
-          @move_fares << @fare_return
-        # else
-        #   @arrive << "unknown"
-        #   @move_fares << "unknown"
-        # end
-        
+        @fare = @elements[0].inner_text.delete("円").delete(",")
+        @fare = @fare.to_i
+        @fare_return = @fare *2
+        @move_fares << @fare_return
       end
       
       @hotel_name.zip(@hotel_address,@hotel_detail,@hotel_prefecture,@hotel_pictureurl,@hotel_sampleratefrom,@arrive,@move_fares).each do|name,address,detail,prefecture,pictureurl,sampleratefrom,arr,fare|
