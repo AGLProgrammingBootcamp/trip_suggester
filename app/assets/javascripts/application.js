9131f74835659ad961cba990ae9fef5f67660f10
H.Yoshiki <vitam.regit.fortuna@gmail.com>
2016-03-27 07:04:27 +0000

    brush up start.html.erb design

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,26 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+//= require_tree ../../../vendor/assets/javascripts/
+
+//= require_directory .
+
+var bright = document.getElementById("bright");
+
+bright.onchange = function(){
+        var b_v = bright.value,
+            max = bright.max,
+            range_x = bright.clientWidth;
+        var b_r =  (range_x / max);
+        b_r = (b_r*b_v);
+        var bright_bar = document.getElementById("bright_bar");
+        bright_bar.style.width = b_r+"px";
+        change_img.style.webkitFilter = "brightness("+b_v+"%)";
+    
+};
+
+function changeBarValue(value) {
+    document.getElementById("bar_val").innerHTML = value;
+};
